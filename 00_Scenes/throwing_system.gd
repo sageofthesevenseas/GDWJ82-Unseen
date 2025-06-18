2ebcf1834c62e1c24c286889093818286c1738b9
JonPita <124646591+JonPita@users.noreply.github.com>
2025-06-18 17:24:09 +1200

    Revert "LZB [006] small changes to chest and adding bomb prefab"
    This reverts commit 3da4277f00cfcb9a642eb7d8a52aa35a7512be86.

--- 00_Scenes/throwing_system.gd
+++ 00_Scenes/throwing_system.gd
@@ -1,13 +1,7 @@
 extends Node2D
 @onready var projectile_spawn: Marker2D = $ProjectileSpawn
 @onready var trajectory_preview: Line2D = $TrajectoryPreview
-@onready var projectile_holdup: Sprite2D = $"Projectile Holdup"
 
-@export var bomb_image: Texture2D
-@export var flare_image: Texture2D
-@export var bomb_prefab: PackedScene
-@export var flare_prefab: PackedScene
-const THROW_MARKER = preload("res://ItemTextures/Throw Marker.png")
 var max_radius := 600.0
 
 func _process(delta: float) -> void:
@@ -19,15 +13,3 @@ func _process(delta: float) -> void:
 		projectile_spawn.position = mouse_pos.normalized() * max_radius
 	else:
 		projectile_spawn.position = mouse_pos
-	if Input.is_action_pressed("throw"):
-		print("winding up throw ")
-		projectile_spawn.visible = true
-		projectile_holdup.visible = true
-		projectile_holdup.texture = bomb_image
-		
-	
-	if Input.is_action_just_released("throw"):
-		print("no longer winding up, throw!")
-		projectile_spawn.visible = false
-		projectile_holdup.visible = false
-		
--- Scripts/chest_minigame_manager.gd
+++ Scripts/chest_minigame_manager.gd
@@ -4,12 +4,10 @@ class_name ChestMinigameManager extends Node2D
 @onready var mini_game_3: Path2D = $MiniGame3
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-@export var DEBUG_run_mini_on_ready: bool = false
 signal chest_game_beaten
 
 func _ready() -> void:
-	if DEBUG_run_mini_on_ready == true:
-		start_minigame()
+	start_minigame()
 
 #func start_minigame(relevant_hidden_chest : HiddenChest) -> void:
 func start_minigame() -> void:
@@ -32,8 +30,5 @@ func _on_mini_game_2_minigame_completed() -> void:
 
 func _on_mini_game_3_minigame_completed() -> void:
 	print("player has completed minigame!")
-	mini_game_1.visible = false
-	mini_game_2.visible = false
-	mini_game_3.visible = false
 	animation_player.play("Open")
 	chest_game_beaten.emit()
