e105f673746ab284a287294daa35f5f3e5873453
Ced <cnolin03@gmail.com>
2025-06-19 12:43:45 -0400

    MenuMusicBugFix

--- 00_Scenes/GameController.gd
+++ 00_Scenes/GameController.gd
@@ -29,6 +29,7 @@ func _ready() -> void:
 	GuiNode.add_child(Main_Menu)
 	var MenuNode = get_node("GUI/UI_Handler")
 	MenuNode.connect("start_game", Callable(self, "_on_start_game"))
+	$FmodEventEmitter2D_Menu.play()
 	pass
 
 func add_lore(index : int) -> void:
@@ -44,7 +45,14 @@ func _on_start_game():
 	WorldNode.add_child(Level)
 	spawn_player_and_switch_camera()
 	GuiNode.remove_child(MenuNode)
-
+	$FmodEventEmitter2D_Menu.set_parameter("GameStart", 1)
+	$Timer.start()
+	pass
+	
+func _on_timer_timeout() -> void:
+		$FmodEventEmitter2D_Cave.play()
+		pass
+	
 func spawn_player_and_switch_camera():
 	var WorldNode = get_node("World2D")
 	var GuiNode = get_node("GUI")
@@ -56,5 +64,5 @@ func spawn_player_and_switch_camera():
 	var camera_instance = camera.instantiate()
 	Player.add_child(camera_instance)
 	camera_instance.make_current()
-	$FmodEventEmitter2D.play()
 	pass
+	
--- Scripts/MenuMusicScriptgd.gd
+++
@@ -1,7 +0,0 @@
-extends FmodEventEmitter2D
-
-func _ready() -> void:
-	var MenuNode = get_node("../")
-	MenuNode.connect("start_game", Callable(self, "_on_start_game"))
-func _on_start_game ():
-	self.set_parameter("GameStart", 1)
