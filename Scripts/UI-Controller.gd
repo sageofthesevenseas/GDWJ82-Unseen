09516e85abf976860b490afcc4761d9bbc84d3c0
sageofthesevenseas <136645913+sageofthesevenseas@users.noreply.github.com>
2025-06-21 00:06:27 +0200

    Ui Edits

--- Scripts/UI-Controller.gd
+++ Scripts/UI-Controller.gd
@@ -3,7 +3,14 @@ extends Control
 
 signal start_game
 signal play_sound(sfx_name)
-	
+var not_in_main_menu = false
+var menu_open = false
+
+func _on_ready():
+	var not_in_main_menu = false
+	var menu_open = false
+
+
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
@@ -11,6 +18,7 @@ func _on_game_start_pressed() -> void:
 	emit_signal("play_sound", "accept")
 	emit_signal("start_game")
 	print("Signal emmitted")
+	not_in_main_menu = true
 	GameController.instance.add_lore(0)
 
 func _on_credits_pressed() -> void:
@@ -42,6 +50,27 @@ func _on_journals_pressed() -> void:
 func _on_fx_h_slider_value_changed(value: float) -> void:
 	emit_signal("play_sound", "accept")
 
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Escape"):
+		print("menu call recieved")
+	if Input.is_action_just_pressed("Escape") and not_in_main_menu and menu_open == false:
+		var player_position = get_node("root/GameController/World2D/CharacterBody2D")
+		self.position = player_position.position
+		self.visible = true
+		$Main.visible = true
+		$Main/GameStart.visible = false
+		menu_open = true
+		print("menu closed")
+	elif Input.is_action_just_pressed("Escape") and not_in_main_menu and menu_open:
+		self.visible = false
+		$Main.visible = false
+		$Main/GameStart.visible = true
+		menu_open = false
+		print("menu closed")
+		
+		
+	
 
 func _on_button_1_pressed() -> void:
 	GameController.instance.add_lore(0)
